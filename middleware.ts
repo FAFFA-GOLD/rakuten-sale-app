fbdf8a442864a6aef8bedcb6d20f11d650af5617
RakutenAppAdmin <admin@example.com>
2025-11-26 15:08:35 +0900

    fix middleware export

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,11 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
 
-// ログイン認証をかけたいページの設定
-// loginページ、api、画像ファイル等以外はすべて認証が必要にする
+export default withAuth({
+  pages: {
+    signIn: "/login", // ログインページの場所を明示
+  },
+});
+
 export const config = {
   matcher: ["/((?!login|api|_next/static|_next/image|favicon.ico).*)"],
 };
